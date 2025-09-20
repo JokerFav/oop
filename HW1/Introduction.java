54399826b7763daebdb90ccfbc4e1183b039eb25
JokerFav <tranvietbao2006@gmail.com>
2025-09-20 20:30:21 +0700

    add Introduction.java

---
+++ HW1/Introduction.java
@@ -0,0 +1,10 @@
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("Họ và tên: Trần Việt Bảo \t MSV: 24020040 \t Lớp: 2526I_INT2204_4 \t Username: JokerFav \t Gmail: tranvietbao2006@gmail.com");
+        for (int i = 9; i > 0; i--) {
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer. ");
+            System.out.println(" Take one down, pass it around, ");
+        }
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
